4dbae6a903a787a347687045f4eae9004bd2b51c
Ravi-Rathod143 <ravikantrathod143@gmail.com>
2025-07-05 13:07:32 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //  only this CORS block:
 const corsOptions = {
-  origin: ["https://jhgjhgj.netlify.app", "http://localhost:5173"],
+  origin: ["https://heymama.netlify.app/", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -18,7 +18,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+// app.options("*", cors(corsOptions));
 
 
 // Routes
